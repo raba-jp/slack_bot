2e3cdaab0298f7ed9b9ce23a605e42567aa65045
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2017-11-02 02:00:14 +0900

    Implement subscribe/unsubscribe

--- twitter/stream.go
+++ twitter/stream.go
@@ -7,6 +7,8 @@ import (
 	"github.com/ChimeraCoder/anaconda"
 )
 
+var finish = make(chan bool)
+
 func getClient() anaconda.TwitterApi {
 	consumerKey := os.Getenv("TWITTER_CONSUMER_KEY")
 	consumerKeySecret := os.Getenv("TWITTER_CONSUMER_KEY_SECRET")
@@ -22,20 +24,41 @@ func getClient() anaconda.TwitterApi {
 	return api
 }
 
-func ListenUserStream() chan Tweet {
+func handle(stream *anaconda.Stream, c chan Tweet) {
+	select {
+	case item := steam.C:
+		switch status := item.(type) {
+		case anaconda.Tweet:
+			t := parseTweet(status)
+			c <- t
+		default:
+			// nop
+		}
+	}
+}
+
+func SubscribeUserStream() chan Tweet {
 	c := make(chan Tweet)
-	steam := getClient().UserStream(url.Values{})
 	go func() {
 		for {
+			handle(getClient().UserStream(url.Values{}), c)
+
 			select {
-			case item := steam.C:
-				switch status := item.(type) {
-				case anaconda.Tweet:
-					parseTweet(status)
+			case f := <-finish:
+				if f {
+					close(c)
+					return
 				}
+			default:
+				// nop
 			}
 		}
 	}()
+	return c
+}
+
+func UnsubscribeUserStream() {
+	finish <- true
 }
 
 func main() {
